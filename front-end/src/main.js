7e68caade3978c7e5490875867f2fc86e2990ce2
codinscode <heronnestg@hotmail.com>
2021-07-29 17:55:04 -0300

    erros

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,11 @@
+/* eslint-disable */ 
 import { createApp } from 'vue';
 import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue';
 import router from './router';
 /* import 'sweetalert2/dist/sweetalert2.min.css'; */
+
+import '../node_modules/nprogress/nprogress.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).use(router, VueSweetalert2).mount('#app');
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,4 +1,7 @@
+/* eslint-disable */ 
 import { createRouter, createWebHistory } from 'vue-router';
+import NProgress from 'nprogress';
+
 
 const routes = [
   {
@@ -22,5 +25,17 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
+router.beforeResolve((to, from, next) => {
+  if (to.name) {
+    NProgress.start();
+  }
+  next();
+});
+
+router.afterEach((to, from) => {
+  NProgress.done();
+});
+
 
 export default router;
