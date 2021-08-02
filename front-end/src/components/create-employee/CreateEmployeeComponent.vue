8debaa5b0e198d0361ac11fe3defdbbdfbf1882a
codinscode <heronnestg@hotmail.com>
2021-08-02 19:37:59 -0300

    parte 5

--- front-end/src/components/create-employee/CreateEmployeeComponent.vue
+++ front-end/src/components/create-employee/CreateEmployeeComponent.vue
@@ -5,26 +5,26 @@
         <h3>Add Employee</h3>
       </div>
       <div class="card-body">
-        <form v-on:submit.prevent="">
+        <form v-on:submit.prevent="handleSubmitForm()">
           <div class="form-group">
             <label class="font-weight-bold">Nome Empregado</label>
-            <input type="text" class="form-control" placeholder="Digite seu nome" >
+            <input type="text" class="form-control" placeholder="Digite seu nome" v-model="employee.name" required >
           </div>
           <div class="form-group">
             <label class="font-weight-bold">Função de Trabalho</label>
-            <input type="text" class="form-control" placeholder="Digite sua função" >
+            <input type="text" class="form-control" placeholder="Digite sua função" v-model="employee.job_role" required >
           </div>
           <div class="form-group">
             <label class="font-weight-bold">Salário</label>
-            <input type="number" class="form-control" placeholder="Digite seu salário" >
+            <input type="number" class="form-control" placeholder="Digite seu salário" v-model="employee.salary" required >
           </div>
           <div class="form-group">
             <label class="font-weight-bold">Data de nascimento</label>
-            <input type="date" class="form-control" placeholder="YYYY/MM/DD" >
+            <input type="date" class="form-control" placeholder="YYYY/MM/DD" v-model="employee.birth" required >
           </div>
           <div class="form-group">
             <label class="font-weight-bold">Registro de empregado</label>
-            <input type="text" class="form-control" placeholder="Registro de empregado" >
+            <input type="text" class="form-control" placeholder="Registro de empregado" v-model="employee.employee_registration" required >
           </div>
 
           <div class="form-group">
@@ -42,8 +42,13 @@ export default {
      name: 'CreateEmployeeComponent'
   },
   data() {
-    return "oi";
+    return {
+       employee: { name: '', job_role: '', salary: '', birth: '', employee_registration: '' }
+    }
   },
+  methods: {
+     handleSubmitForm() {}
+  }
 };
 </script>
 
