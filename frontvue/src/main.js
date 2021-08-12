bbbb93fabda9e62e208e2909d506d66070d78f3f
codingscode <heronnestg@hotmail.com>
2021-08-11 21:50:42 -0300

    parte 3

--- frontvue/src/main.js
+++ frontvue/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//import VueSweetalert2 from 'vue-sweetalert2';
+//import router from './router';
+
+//import '../node_modules/nprogress/nprogress.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
+
