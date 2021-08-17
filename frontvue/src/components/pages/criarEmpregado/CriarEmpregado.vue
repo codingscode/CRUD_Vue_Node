164b305a7597f9c5ace0306420dacd6d44a1a46d
codingscode <heronnestg@hotmail.com>
2021-08-17 20:35:03 -0300

    parte 7

--- frontvue/src/components/pages/criarEmpregado/CriarEmpregado.vue
+++ frontvue/src/components/pages/criarEmpregado/CriarEmpregado.vue
@@ -8,23 +8,23 @@
            <form @submit.prevent="criarNovoEmpregado()" > 
              <div class="form-group">
                <label class="font-weight-bold">Nome Empregado</label>
-               <input type="text" class="form-control" placeholder="Digite seu nome"  > <!-- v-model="employee.name" required --> 
+               <input type="text" class="form-control" placeholder="Digite seu nome" v-model="empregado.nome" required > 
              </div>
              <div class="form-group">
                <label class="font-weight-bold">Função de Trabalho</label>
-               <input type="text" class="form-control" placeholder="Digite sua função"  >  <!-- v-model="employee.job_role" required -->
+               <input type="text" class="form-control" placeholder="Digite sua função" v-model="empregado.trabalho_funcao" required >  
              </div>
              <div class="form-group">
                <label class="font-weight-bold">Salário</label>
-               <input type="number" class="form-control" placeholder="Digite seu salário"  >  <!-- v-model="employee.salary" required -->
+               <input type="number" class="form-control" placeholder="Digite seu salário" v-model="empregado.salario" required >  
              </div>
              <div class="form-group">
                <label class="font-weight-bold">Data de nascimento</label>
-               <input type="date" class="form-control" placeholder="YYYY/MM/DD"  >  <!-- v-model="employee.birth" required -->
+               <input type="date" class="form-control" placeholder="YYYY/MM/DD" v-model="empregado.nascimento" required > 
              </div>
              <div class="form-group">
                <label class="font-weight-bold">Registro de empregado</label>
-               <input type="text" class="form-control" placeholder="Registro de empregado"  >  <!-- v-model="employee.employee_registration" required -->
+               <input type="text" class="form-control" placeholder="Registro de empregado" v-model="empregado.registro_empregado" required >  
              </div>
              <div class="form-group">
                <button class="btn btn-primary" type="submit" >+ Empregado</button>
@@ -41,8 +41,15 @@ export default {
    name: 'CriarEmpregado',
    data() {
       return {
-         
+         empregado: {
+            nome: '1', trabalho_funcao: '2', salario: '3', nascimento: '4', registro_empregado: '5'
+         }
       }
+   },
+   methods: {
+      gerenSUbmitForm() {
+      
+      } 
    }
 }
 </script>
