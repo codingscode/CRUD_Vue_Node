e2bbc77672abe13d06617b8aed0bbe1a94c5946f
codingscode <heronnestg@hotmail.com>
2021-08-17 20:18:51 -0300

    parte 6

--- frontvue/src/components/pages/criarEmpregado/CriarEmpregado.vue
+++ frontvue/src/components/pages/criarEmpregado/CriarEmpregado.vue
@@ -1,7 +1,38 @@
 <template>
-   <div class="row justify-content-center" >
-      <h1>Criar Novo Empregado</h1>
-   </div>
+   <div class="container">
+      <div class="card">
+         <div class="card-header">
+            <h3>Adicionar novo empregado</h3>
+         </div>
+         <div class="card-body">
+           <form @submit.prevent="criarNovoEmpregado()" > 
+             <div class="form-group">
+               <label class="font-weight-bold">Nome Empregado</label>
+               <input type="text" class="form-control" placeholder="Digite seu nome"  > <!-- v-model="employee.name" required --> 
+             </div>
+             <div class="form-group">
+               <label class="font-weight-bold">Função de Trabalho</label>
+               <input type="text" class="form-control" placeholder="Digite sua função"  >  <!-- v-model="employee.job_role" required -->
+             </div>
+             <div class="form-group">
+               <label class="font-weight-bold">Salário</label>
+               <input type="number" class="form-control" placeholder="Digite seu salário"  >  <!-- v-model="employee.salary" required -->
+             </div>
+             <div class="form-group">
+               <label class="font-weight-bold">Data de nascimento</label>
+               <input type="date" class="form-control" placeholder="YYYY/MM/DD"  >  <!-- v-model="employee.birth" required -->
+             </div>
+             <div class="form-group">
+               <label class="font-weight-bold">Registro de empregado</label>
+               <input type="text" class="form-control" placeholder="Registro de empregado"  >  <!-- v-model="employee.employee_registration" required -->
+             </div>
+             <div class="form-group">
+               <button class="btn btn-primary" type="submit" >+ Empregado</button>
+             </div>
+           </form>
+         </div>
+      </div>
+  </div>
 </template>
 
 <script>
