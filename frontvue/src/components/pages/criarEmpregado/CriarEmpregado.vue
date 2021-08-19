02c8026c41136f0c981563fd51f90f3cd9649b14
codingscode <heronnestg@hotmail.com>
2021-08-18 22:37:36 -0300

    parte 8

--- frontvue/src/components/pages/criarEmpregado/CriarEmpregado.vue
+++ frontvue/src/components/pages/criarEmpregado/CriarEmpregado.vue
@@ -5,26 +5,41 @@
             <h3>Adicionar novo empregado</h3>
          </div>
          <div class="card-body">
-           <form @submit.prevent="criarNovoEmpregado()" > 
+           <form @submit.prevent="gerenSubmitForm()" > 
              <div class="form-group">
                <label class="font-weight-bold">Nome Empregado</label>
-               <input type="text" class="form-control" placeholder="Digite seu nome" v-model="empregado.nome" required > 
+               <input id="nome" name="nome" type="text" class="form-control" placeholder="Digite seu nome" v-model="formEmpregado.nome" :class="{ 'is-invalid': isSubmitted && $v.formEmpregado.nome.$error }" > 
+               <div v-if="isSubmitted && !$v.formEmpregado.nome.required" class="invalid-feedback" >
+                  Campo nome empregado é exigido!
+               </div>
              </div>
              <div class="form-group">
                <label class="font-weight-bold">Função de Trabalho</label>
-               <input type="text" class="form-control" placeholder="Digite sua função" v-model="empregado.trabalho_funcao" required >  
+               <input id="funcao" name="funcao" type="text" class="form-control" placeholder="Digite sua função" v-model="formEmpregado.trabalho_funcao" :class="{ 'is-invalid': isSubmitted && $v.formEmpregado.funcao.$error }" >  
+               <div v-if="isSubmitted && !$v.formEmpregado.funcao.required" class="invalid-feedback" >
+                  Campo função é exigido!
+               </div>
              </div>
              <div class="form-group">
                <label class="font-weight-bold">Salário</label>
-               <input type="number" class="form-control" placeholder="Digite seu salário" v-model="empregado.salario" required >  
+               <input id="salario" name="salario" type="number" class="form-control" placeholder="Digite seu salário" v-model="formEmpregado.salario" :class="{ 'is-invalid': isSubmitted && $v.formEmpregado.salario.$error }" >  
+               <div v-if="isSubmitted && !$v.formEmpregado.salario.required" class="invalid-feedback" >
+                  Campo salário é exigido!
+               </div>
              </div>
              <div class="form-group">
                <label class="font-weight-bold">Data de nascimento</label>
-               <input type="date" class="form-control" placeholder="YYYY/MM/DD" v-model="empregado.nascimento" required > 
+               <input id="nascimento" name="nascimento" type="date" class="form-control" placeholder="YYYY/MM/DD" v-model="formEmpregado.nascimento" :class="{ 'is-invalid': isSubmitted && $v.formEmpregado.nascimento.$error }" > 
+               <div v-if="isSubmitted && !$v.formEmpregado.nascimento.required" class="invalid-feedback" >
+                  Campo nascimento é exigido!
+               </div>
              </div>
              <div class="form-group">
                <label class="font-weight-bold">Registro de empregado</label>
-               <input type="text" class="form-control" placeholder="Registro de empregado" v-model="empregado.registro_empregado" required >  
+               <input id="registro" name="registro" type="text" class="form-control" placeholder="Registro de empregado" v-model="formEmpregado.registro_empregado" :class="{ 'is-invalid': isSubmitted && $v.formEmpregado.nascimento.$error }" >  
+               <div v-if="isSubmitted && !$v.formEmpregado.registro.required" class="invalid-feedback" >
+                  Campo registro é exigido!
+               </div>
              </div>
              <div class="form-group">
                <button class="btn btn-primary" type="submit" >+ Empregado</button>
@@ -37,18 +52,35 @@
 
 <script>
 
+import { required } from 'vuelidate/lib/validators'
+
 export default {
    name: 'CriarEmpregado',
    data() {
       return {
-         empregado: {
+         formEmpregado: {
             nome: '1', trabalho_funcao: '2', salario: '3', nascimento: '4', registro_empregado: '5'
-         }
+         },
+         isSubmitted: false
       }
    },
+   validations: {
+   formEmpregado: {
+      nome: { required },
+      trabalho_funcao: { required },
+      salario: { required },
+      nascimento: { required },
+      registro_empregado: { required }
+   }
+   },
    methods: {
-      gerenSUbmitForm() {
-      
+      gerenSubmitForm() {
+         this.isSubmitted = true
+   
+         this.$v.touch()
+         if (this.$v.invalid) {
+            return
+         }
       } 
    }
 }
--- frontvue/src/main.js
+++ frontvue/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 
 import VueSweetalert2 from 'vue-sweetalert2'
 import router from './router'
+
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/nprogress/nprogress.css'
@@ -11,9 +13,11 @@ import '../node_modules/nprogress/nprogress.css'
 Vue.config.productionTip = false
 
 Vue.use(VueSweetalert2)
+Vue.use(Vuelidate)
+
 
 new Vue({
-  router,
-  render: (h) => h(App),
+   router,
+   render: (h) => h(App),
 }).$mount('#app')
 
